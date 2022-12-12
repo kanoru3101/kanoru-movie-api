d9cda1762f7c6b55f877caa9536d061cda57db67
kanoru <kanoru3101@gmail.com>
2022-12-12 12:16:55 +0200

    test 7

--- src/routes/my.ts
+++ src/routes/my.ts
@@ -4,7 +4,7 @@ const router = Router()
 
 router.get('/', (_req: Request, res: Response): string => {
   res.send('Express + TypeScript Server !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-  return 3
+  return 6
 })
 
 export default router
