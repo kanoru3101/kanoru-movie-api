591e97c1e4762b7e3ff3e660fe6d1cab521eff02
kanoru <kanoru3101@gmail.com>
2022-12-12 12:42:39 +0200

    test 10

--- src/routes/my.ts
+++ src/routes/my.ts
@@ -2,7 +2,8 @@ import { Router, Request, Response } from 'express'
 
 const router = Router()
 
-router.get('/', (_req: Request, res: Response): string => {
+router.get('/', 
+(_req: Request, res: Response): string => {
   res.send('Express + TypeScript Server !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
   return 7
 })
