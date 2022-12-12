17e16a2c7916fe4086ba1e329a579f44226e606e
kanoru <kanoru3101@gmail.com>
2022-12-12 11:56:32 +0200

    test 4

--- src/routes/my.ts
+++ src/routes/my.ts
@@ -3,7 +3,8 @@ import { Router, Request, Response } from 'express'
 const router = Router()
 
 router.get('/', (_req: Request, res: Response): string => {
-  res.send('Express + TypeScript Server !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+  res.send('Express + TypeScript Server !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
+  return 3
 })
 
 export default router
