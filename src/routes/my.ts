0895023f2ac2e528634bd5ba213b603ac4cb61a4
kanoru <kanoru3101@gmail.com>
2022-12-13 12:05:50 +0200

    test precommit last

--- src/routes/my.ts
+++ src/routes/my.ts
@@ -2,9 +2,8 @@ import { Router, Request, Response } from 'express'
 
 const router = Router()
 
-router.get('/', (_req: Request, res: Response): void => {
+router.get('/', (_req: Request, res: Response) => {
   res.send('Express + TypeScript Server !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
-  return 10
 })
 
 export default router
