528b1808fccdece1ef370ec52725ebb2b603db36
kanoru <kanoru3101@gmail.com>
2023-02-03 19:13:17 +0200

    fix

--- src/services/themovie/getTrending.ts
+++ src/services/themovie/getTrending.ts
@@ -17,7 +17,7 @@ const getTrending = async ({
   timeWindow,
 }: Trending): Promise<TrendingResponse> => {
 
-  return await themovieDB({ url: `trending/${mediaType}/${timeWindow}`, language: 'uk'})
+  return await themovieDB({ url: `trending/${mediaType}/${timeWindow}`, language })
 }
 
 export default getTrending
