8ed3b4bc9b393ed8f94141488d7faab467518f44
Viktor <31951646+kanoru3101@users.noreply.github.com>
2023-02-03 19:14:18 +0200

    Add Movie and Genre Model (#6)
    * add changes
    
    * fix

---
+++ src/constants/models.ts
@@ -0,0 +1,13 @@
+export enum LANGUAGES {
+  EN = 'en',
+  UA = 'ua'
+}
+
+export enum MOVIE_STATUSES {
+  RUMORED = 'Rumored',
+  PLANNED = 'Planned',
+  IN_PRODUCTION = 'In Production',
+  POST_PRODUCTION = 'Post Production',
+  RELEASED = 'Released',
+  CANCELED = 'Canceled',
+}
--- src/models/index.ts
+++ src/models/index.ts
@@ -1,4 +1,8 @@
 import User from './user'
 import Log from './log';
+import Genre from './genre';
+import Movie from './movie';
 
-export { User, Log }
+export {
+  User, Log, Genre, Movie
+}
